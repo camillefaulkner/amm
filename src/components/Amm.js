6d6015430a202f8bf8dd895aeacab8a90decccff
Camille Faulkner <camillecfaulkner@gmail.com>
2023-01-31 14:43:07 -0600

    mmm yes its begun

---
+++ src/components/Amm.js
@@ -0,0 +1,3 @@
+export const Amm = () => {
+    return <>u want amm</>
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+export const Nav = () => {
+    return <ul className="nav">
+        <li>home</li>
+        <li>about the amms</li>
+        <li>press</li>
+        <li>case studies</li>
+        <li>contact</li>
+        </ul>
+}
