b0c681f612f19d70989d3379563ed133779eb2a1
Camille Faulkner <camillecfaulkner@gmail.com>
2023-02-04 21:40:10 -0600

    typo

--- src/components/Press.js
+++ src/components/Press.js
@@ -4,7 +4,7 @@ export const Press = () => {
     return <div className="page">
         <div>best show of the year 2022 - Third Coast Comedy Club, Nashville, TN</div> <br/>
     <div>local artist/band that is going to rule in 2023 - Nashville Scene <br/>
-        <a href="https://www.nashvillescene.com/music/coverstory/the-2022-rock-n-roll-poll/article_de098794-7b19-11ed-9701-1b2eac07039a.html" target="_blank">divnk to article</a>
+        <a href="https://www.nashvillescene.com/music/coverstory/the-2022-rock-n-roll-poll/article_de098794-7b19-11ed-9701-1b2eac07039a.html" target="_blank">link to article</a>
     </div>
     </div>
 }
